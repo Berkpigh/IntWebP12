1466f40432dfeae4a0b740c17d83966d7ad4c0b1
Berkpigh <pierre.berkovits@netplus.ch>
2024-03-12 16:13:44 +0100

    Tri des périodes

--- client/src/components/Periode.jsx
+++ client/src/components/Periode.jsx
@@ -1,23 +1,4 @@
-import PropTypes from 'prop-types'
-import ima1 from '/1bitMageticCoreMemory.png'
-import ima2 from '/2TimeSharing.png'
-import ima3 from '/3features-of-c.png'
-import ima4 from '/4thm_IntroToVAXVMS.jpg'
-import ima5 from '/5MacIntosh.jpeg'
-import ima6 from '/WIN_20240307_16_16_30_Pro.jpg'
-import ima7 from '/7NexusBrowser.jpeg'
-import ima8 from '/8SimCity.jpg'
-import ima9 from '/9OpenSource.jpg'
-
-const propTypes = {
-  titre: PropTypes.string,
-  caracteristique: PropTypes.string,
-  vecu: PropTypes.string,
-  numImage: PropTypes.number,
-  image: PropTypes.string,
-  descriptionImage: PropTypes.string,
-}
-const defaultProps = { String }
+import propTypes from 'prop-types'
 
 function Periode({
   titre,
@@ -27,9 +8,6 @@ function Periode({
   image,
   descriptionImage,
 }) {
-  const imaArray = [ima1, ima2, ima3, ima4, ima5, ima6, ima7, ima8, ima9]
-  const nuim = numImage
-  //const ima = imaArray[nuim]
   const ima = { image }.image
   console.log(ima)
   return (
@@ -42,6 +20,13 @@ function Periode({
     </div>
   )
 }
-Periode.propTypes = propTypes
-Periode.defaultProps = defaultProps
+Periode.propTypes = {
+  titre: propTypes.string,
+  caracteristique: propTypes.string,
+  vecu: propTypes.string,
+  numImage: propTypes.number,
+  image: propTypes.string,
+  descriptionImage: propTypes.string,
+}
+
 export default Periode
--- client/src/pages/Passe.jsx
+++ client/src/pages/Passe.jsx
@@ -3,11 +3,15 @@ import Periode from '../components/Periode'
 
 const Passe = () => {
   const { currentEpoque } = useSelector((state) => state.epoque)
-  console.log({ currentEpoque })
+  const ce = { currentEpoque }.currentEpoque
+
+  const ceSorted = ce.sort((p1, p2) => p1.numImage - p2.numImage)
+  //console.log('ceSorted : ', ceSorted)
+
   return (
     <div className="flex flex-col">
       <p>Hello mon passé</p>
-      {currentEpoque.map(
+      {ceSorted.map(
         ({
           _id,
           titre,
